1b19669abded8bf58c885ed0474f8cb992a4a7ec
joaoterroa <joaoterroa2@gmail.com>
2023-07-03 19:32:50 +0100

    fixed default react component in utils

--- pages/utils.js
+++ pages/utils.js
@@ -1,19 +1,22 @@
 const fetchWeatherData = async (latitude, longitude, apiKey) => {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
-    );
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+        );
 
-    if (response.ok) {
-      const weatherData = await response.json();
-      return weatherData;
-    } else {
-      throw new Error(`Error fetching weather data: ${response.statusText}`);
+        if (response.ok) {
+            const weatherData = await response.json();
+            return weatherData;
+        } else {
+            throw new Error(
+                `Error fetching weather data: ${response.statusText}`
+            );
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
     }
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
 };
 
-export { fetchWeatherData };
+// export { fetchWeatherData };
+export default fetchWeatherData;
